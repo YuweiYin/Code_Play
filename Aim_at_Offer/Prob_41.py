45753a0cdd6fc240f6f3aa694337fe8ee248d4ff
YuweiYin <seckexyin@gmail.com>
2019-05-24 08:44:57 +0800

    Aim_at_Offer Prob_41 Add a Method

--- Aim_at_Offer/Prob_41.py
+++ Aim_at_Offer/Prob_41.py
@@ -21,6 +21,7 @@ Good Luck!
 '''
 import sys
 import getopt
+import copy
 
 
 class Solution:
@@ -32,35 +33,69 @@ class Solution:
         if tsum == 3:
             return [[1, 2]]
 
-        # 方法一：O(n^2)蛮力计算
         answer = [] # 最终答案
         seq = [] # 当前序列
         seq_sum = 0 # 当前序列的值总和
         end = tsum / 2 + 1 # 最多找到 1/2 值加 1 的位置就够了
 
-        i = 1
-        while i <= end:
-            seq = []
-            seq_sum = 0
+        # 方法一：O(n^2)蛮力计算
+        # i = 1
+        # while i <= end:
+        #     seq = []
+        #     seq_sum = 0
 
-            # 从当前 i 的位置一个个往后加起来
-            j = i
-            while j <= end:
-                seq.append(j)
-                seq_sum += j
+        #     # 从当前 i 的位置一个个往后加起来
+        #     j = i
+        #     while j <= end:
+        #         seq.append(j)
+        #         seq_sum += j
 
-                # 恰好达到了目标值 tsum，记录序列并退出内循环
-                if seq_sum == tsum:
-                    answer.append(seq)
-                    break
-                # 超过了目标值，直接退出内循环
-                elif seq_sum > tsum:
-                    break
-                # 还未达到目标值，继续下一轮内循环
-                else:
-                    j += 1
+        #         # 恰好达到了目标值 tsum，记录序列并退出内循环
+        #         if seq_sum == tsum:
+        #             answer.append(seq)
+        #             break
+        #         # 超过了目标值，直接退出内循环
+        #         elif seq_sum > tsum:
+        #             break
+        #         # 还未达到目标值，继续下一轮内循环
+        #         else:
+        #             j += 1
 
-            i += 1
+        #     i += 1
+
+        # return answer
+
+        # 方法二：区间夹逼
+        small = 1 # 区间下限
+        big = 2 # 区间上限
+
+        seq.append(1)
+        seq.append(2)
+        seq_sum = 3
+        while small < end:
+            # print small, big, seq_sum
+
+            # 当前结果相比目标值，小则 big 后移，大则 small 后移
+            if seq_sum < tsum:
+                big += 1
+                seq_sum += big
+                seq.append(big)
+
+            elif seq_sum > tsum:
+                small += 1
+                pop = seq.pop(0)
+                seq_sum -= pop
+
+            # 当前结果跟目标值相同
+            else:
+                # print seq
+                # 注意要深复制后加入 answer，
+                # 不能仅是 answer.append(seq)
+                answer.append(copy.deepcopy(seq))
+
+                small += 1
+                pop = seq.pop(0)
+                seq_sum -= pop
 
         return answer
 
@@ -82,7 +117,7 @@ def main(argv=None):
 
     solution = Solution()
 
-    # tsum = 11
+    # tsum = 10
     tsum = 100
     # tsum = 300
 
