374f451e683f28e837c50d9d466291e5637368de
YuweiYin <seckexyin@gmail.com>
2019-06-21 23:27:18 +0800

    ByteCamp 2019 Alg. 4

--- Introduction_To_Algorithms/Introduction_To_Algorithms/main.cpp
+++ Introduction_To_Algorithms/Introduction_To_Algorithms/main.cpp
@@ -6,10 +6,92 @@
 //  Copyright © 2019 阴昱为. All rights reserved.
 //
 
+// 设置系统栈深度
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+
+// 引入头文件
 #include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+
+#include <math.h>
+#include <time.h>
+
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <bitset>
+
+#include <unordered_set>
+#include <unordered_map>
+
+// 使用 std 标准命名空间
+using namespace std;
+
+// 类型命名
+//typedef __int64_t ll;
+//#define ll __int64_t
+//#define ll long long
+
+// 全局常量
+//#define PI acos(-1.0)
+//const double EPS = 1e-14;
+//const ll MAX = 1ll<<55;
+//const double INF = ~0u>>1;
+//const int MOD = 1000000007;
+
+//const int MAX_INT32 = 0x7fffffff;
+//const int MIN_INT32 = -0x80000000;
+//const ll MAX_INT32 = 2147483647;
+//const ll MIN_INT32 = -2147483648;
+
+
+class Solution {
+public:
+    Solution () {
+        
+    }
+    
+    bool compareTwoNumbers (int num1, int num2) {
+        return this->solution1(num1, num2);
+    }
+    
+private:
+    bool solution1 (int num1, int num2) {
+        return num1 < num2;
+    }
+};
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // 计时
+    time_t start, finish;
+    double prog_duration;
+    start = clock();
+    
+    // 设置测试数据
+    int num1 = 1;
+    int num2 = 2;
+    
+    // 调用解决方案，获得处理结果，并输出展示结果
+    Solution *solution = new Solution();
+    bool ans = solution->compareTwoNumbers(num1, num2);
+    if (ans) {
+        cout << num1 << " is less than " << num2 << endl;
+    } else {
+        cout << num1 << " is larger than " << num2 << endl;
+    }
+    
+    // 程序执行时间
+    finish = clock();
+    prog_duration = (double)(finish - start) * 1000 / CLOCKS_PER_SEC;
+    cout << "程序执行时间: " << prog_duration << "ms." << endl;
+    
     return 0;
 }
