513b30cc268b8f2254c04b6a98ec6ac699198d81
YuweiYin <seckexyin@gmail.com>
2019-06-27 21:26:09 +0800

    LeetCode 1996 Number of Squareful Arrays

---
+++ Basis/Basis/Sort_Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sort_Algorithm
+//
+//  Created by 阴昱为 on 2019/6/26.
+//  Copyright © 2019 阴昱为. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
