44400ae5d97227aa0032039d596a0aefac5f7045
YuweiYin <seckexyin@gmail.com>
2019-07-21 22:58:28 +0800

    LeetCode 55 Jump Game

---
+++ LeetCode/LeetCode/Prob1045_Jump_Game_II/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Prob1045_Jump_Game_II
+//
+//  Created by 阴昱为 on 2019/7/21.
+//  Copyright © 2019 阴昱为. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
