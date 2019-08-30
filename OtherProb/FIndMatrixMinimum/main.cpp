80b166a67364337238abf723f6bc819ff8545454
YuweiYin <seckexyin@gmail.com>
2019-08-30 22:21:35 +0800

    Find a Minimum in a Matrix

---
+++ OtherProb/FIndMatrixMinimum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FIndMatrixMinimum
+//
+//  Created by 阴昱为 on 2019/8/30.
+//  Copyright © 2019 阴昱为. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
