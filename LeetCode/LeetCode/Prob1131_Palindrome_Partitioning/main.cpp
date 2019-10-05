69b3816e17ba6b825476ee4cc56cf4fb66324632
YuweiYin <seckexyin@gmail.com>
2019-10-05 23:09:34 +0800

    LeetCode 125 Valid Palindrome

---
+++ LeetCode/LeetCode/Prob1131_Palindrome_Partitioning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Prob1131_Palindrome_Partitioning
+//
+//  Created by 阴昱为 on 2019/10/2.
+//  Copyright © 2019 阴昱为. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
